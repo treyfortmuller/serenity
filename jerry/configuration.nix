c9e9f51f904bd3b89da4e9078a48c6c308790a27
Trey Fortmuller <tfortmuller@mac.com>
2025-11-30 21:16:41 -0800

    setup pi user identity file for age

--- jerry/configuration.nix
+++ jerry/configuration.nix
@@ -13,7 +13,12 @@
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
 
-  age.secrets.openweather.file = ../secrets/openweather.age;
+  age = {
+    identityPaths = [
+      "/home/pi/.ssh/id_ed25519"
+    ];
+    secrets.openweather.file = ../secrets/openweather.age;
+  };
 
   serenity = {
     localDev = {
